d55a669deffdbd1d2c9abf557dfd5598e49987c0
Голиков Ярослав <y.golikov@kvantorium62.local>
2020-09-24 19:51:26 +0300

    vk stranitsa

--- src/App/App.js
+++ src/App/App.js
@@ -2,7 +2,13 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="wrapper"></div>
+      <div className="wrapper1"></div>
+      <div className="wrapper2"></div>
+    </div>
+  );
 }
 
 export default App;
