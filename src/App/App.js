dc628f113b67ca7bd67442a84e685896eb0269b6
Голиков Ярослав <y.golikov@kvantorium62.local>
2020-09-29 19:48:51 +0300

    created top stuku

--- src/App/App.js
+++ src/App/App.js
@@ -4,9 +4,35 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <div className="wrapper"></div>
-      <div className="wrapper1"></div>
-      <div className="wrapper2"></div>
+      <div className="vsy">
+        <div className="Slova">
+          <p>
+            <h1>Откуда мемы лучше</h1>
+          </p>
+        </div>
+
+        <p>
+          <button
+            onClick={() => {
+              document.location.href =
+                "http://risovach.ru/upload/2014/03/mem/putin_46200719_orig_.jpg";
+            }}
+            className="Tik"
+          >
+            <p className="odin">Тик-</p>
+            <p className="dva">ток</p>
+          </button>
+          <button
+            onClick={() => {
+              document.location.href =
+                "https://risovach.ru/upload/2015/12/mem/putin_100431093_orig_.jpg";
+            }}
+            className="vk"
+          >
+            <h4>vk</h4>
+          </button>
+        </p>
+      </div>
     </div>
   );
 }
