cc8c114c0e956749a81ab2721200aa9ed3ec2f1f
Голиков Ярослав <y.golikov@kvantorium62.local>
2020-10-13 19:52:07 +0300

    add keyPressHandler

--- src/App/App.js
+++ src/App/App.js
@@ -11,12 +11,29 @@ class App extends React.Component {
     };
   }
 
+  keyPressHandler({ key }) {
+    if (key === this.state.text.substr(this.state.currentLetterNumber, 1)) {
+      this.setState({
+        currentLetterNumber: this.state.currentLetterNumber + 1,
+      });
+    }
+    console.log(key);
+  }
+
+  componentDidMount() {
+    document.onkeypress = this.keyPressHandler.bind(this);
+  }
+
   render() {
     return (
       <div className="App">
         <div className="text">
-          <span className="completed"></span>
-          <span className="uncompleted">{this.state.text}</span>
+          <span className="completed">
+            {this.state.text.substr(0, this.state.currentLetterNumber)}
+          </span>
+          <span className="uncompleted">
+            {this.state.text.substr(this.state.currentLetterNumber)}
+          </span>
         </div>
       </div>
     );
